dae6bf3e79dd0022757cb26be9252e87fb924b38
Daniel Mathiot <d.danymat@gmail.com>
2021-04-19 17:56:29 +0200

    feat: Added dataframe reading from json
    +feat: Added test folder

--- insanonym_utils/modules/Dataframe.py
+++ insanonym_utils/modules/Dataframe.py
@@ -1,8 +1,12 @@
 from os import path
+from pandas import read_json
 from ..models import FileConfigModel, FileModel
 
 class FileAsDataframe:
 
     def __init__(self, file: FileConfigModel):
-        file = FileModel(name=file.name, path=path.join(file.path, file.name))
+        _file = FileModel(name=file.name, path=path.join(file.path, file.name))
+        self._dataframe = read_json(path_or_buf=_file.path)
+        print(self._dataframe)
+
 
