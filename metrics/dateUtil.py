b4380c3e4a894fc96996ca104ba883db053e75ab
quocdat-le-insacvl <71183203+quocdat-le-insacvl@users.noreply.github.com>
2021-11-10 12:53:56 +0100

    Refactor date_util

--- metrics/dateUtil.py
+++ metrics/dateUtil.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
 def main(df_anon, df_original, params, nb_orig_lines):
+    df = df_anon.copy()
+    df_original_reduced = df_original.copy()
+    
     # Verify if the weeks are the same
-    if not (df_anon['date'].dt.isocalendar().week == df_original['date'].dt.isocalendar().week).all():
+    if not (df['date'].dt.isocalendar().week == df_original_reduced['date'].dt.isocalendar().week).all():
         raise Exception("Weeks must be the same")
 
-    df_score = 3 - abs(df_anon.date.dt.dayofweek - df_original.date.dt.dayofweek)
+    df_score = 3 - abs(df.date.dt.dayofweek - df_original_reduced.date.dt.dayofweek)
 
     return df_score.loc[df_score > 0].sum() / 3 / nb_orig_line
